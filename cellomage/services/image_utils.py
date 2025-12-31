0e1f30fc3cc51a333ea8cdd2fa4329e654c786e3
Aviseka00 <chimericwellness@gmail.com>
2025-12-31 22:24:50 +0530

    Update cellomage: restructure project files

--- cellomage/services/image_utils.py
+++
@@ -1,9 +0,0 @@
-import os
-import uuid
-
-def save_image(file, folder):
-    os.makedirs(folder, exist_ok=True)
-    filename = f"{uuid.uuid4()}.jpg"
-    path = os.path.join(folder, filename)
-    file.save(path)
-    return filename, path
